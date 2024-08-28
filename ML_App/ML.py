bd9992715369973154656d7432070344c6d08c32
Abdul Samad <158138089+abdulsamad577@users.noreply.github.com>
2024-08-28 11:38:02 +0500

    ML.py

--- ML_App/ML.py
+++ ML_App/ML.py
@@ -15,7 +15,7 @@ from sklearn.decomposition import PCA
 #Heading...
 st.header("Welcome to Data Scientist, Samad Platform!")
 st.write("---")
-image1=Image.open("/ML_App/pic.jpg")
+image1=Image.open("ML_App/pic.jpg")
 st.image(image1)
 
 st.write("---")
